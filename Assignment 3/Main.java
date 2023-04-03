671b6ffe0e100b3f8a0cff25c8b5454cfd26e1f9
Pranav Kulkarni <66161105+pranavkulkarni2905@users.noreply.github.com>
2023-04-03 12:37:01 +0530

    Create Main.java

---
+++ Assignment 3/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rectangle r=new Rectangle();
+		System.out.println("Area of rectnagle is :"+r.area());
+		
+		Triangle t=new Triangle();
+		System.out.println("Area of triangle is :"+t.area());
+		
+		Square s=new Square();
+		System.out.println("Area of square is :"+s.area());
+
+	}
+
+}
